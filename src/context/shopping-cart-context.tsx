04fdd4893c1b580845156c6832dd111ee4eb3e57
BR-Darkness <BRvitorgalindo@gmail.com>
2024-05-11 10:34:39 -0300

    feat: Add useLocalStorage hook

--- src/context/shopping-cart-context.tsx
+++ src/context/shopping-cart-context.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useState } from "react";
 import { toast } from "sonner";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode;
@@ -26,7 +27,10 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children } : ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+        "shopping-cart-local-storage",
+        []
+    )
 
     function getCartQuantity() {
         return cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
