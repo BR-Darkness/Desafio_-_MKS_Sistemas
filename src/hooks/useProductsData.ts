d87ecce7cae22f91640275be185625c5e068f992
BR-Darkness <BRvitorgalindo@gmail.com>
2024-09-10 17:13:19 -0300

    [feat, fix] add static json api files (2/2)

--- src/hooks/useProductsData.ts
+++ src/hooks/useProductsData.ts
@@ -2,7 +2,7 @@ import { IProductDataResponse } from "../interfaces/product.interface";
 import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
 
-const API_URL = "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC";
+const API_URL = "https://raw.githubusercontent.com/BR-Darkness/Desafio_-_MKS_Sistemas/main/src/data/data.json";
 
 const fetchData = async (): AxiosPromise<IProductDataResponse> => {
     const response = await axios.get<IProductDataResponse>(API_URL);
